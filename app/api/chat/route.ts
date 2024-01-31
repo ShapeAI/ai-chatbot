7f8d247c92cd0adb2e97389d30d863989ac8d1a7
David Ragones <dragones@gmail.com>
2024-01-31 15:33:48 -0800

    close SHAPE-158, SHAPE-159, SHAPE-160

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,65 +1,77 @@
-import { kv } from '@vercel/kv'
-import { OpenAIStream, StreamingTextResponse } from 'ai'
-import OpenAI from 'openai'
+import { NextRequest } from 'next/server';
+import { Message as VercelChatMessage, StreamingTextResponse } from 'ai';
+ 
+import { Chroma } from "@langchain/community/vectorstores/chroma";
+import { BytesOutputParser } from '@langchain/core/output_parsers';
+import { PromptTemplate } from '@langchain/core/prompts';
+import { Ollama } from "@langchain/community/llms/ollama";
+import { OpenAIEmbeddings } from "@langchain/openai";
 
-import { auth } from '@/auth'
-import { nanoid } from '@/lib/utils'
 
-export const runtime = 'edge'
+export const runtime = 'edge';
+ 
+/**
+ * Basic memory formatter that stringifies and passes
+ * message history directly into the model.
+ */
+const formatMessage = (message: VercelChatMessage) => {
+  return `${message.role}: ${message.content}`;
+};
+ 
+const ollama = new Ollama({
+    baseUrl: "http://localhost:11434", // Default value
+    model: "mistral", // Default value
+  });
 
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY
-})
+const TEMPLATE = `Answer the question: {query} based on the following documents and conversation.
 
-export async function POST(req: Request) {
-  const json = await req.json()
-  const { messages, previewToken } = json
-  const userId = (await auth())?.user.id
+Documents:
+{documents}
 
-  if (!userId) {
-    return new Response('Unauthorized', {
-      status: 401
-    })
-  }
+Current conversation:
+{chat_history}`;
+ 
+/*
+ * This handler initializes and calls a simple chain with a prompt,
+ * chat model, and output parser. See the docs for more information:
+ *
+ * https://js.langchain.com/docs/guides/expression_language/cookbook#prompttemplate--llm--outputparser
+ */
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  const messages = body.messages ?? [];
+  const formattedPreviousMessages = messages.slice(0, -1).map(formatMessage);
+  const currentMessageContent = messages[messages.length - 1].content;
+ 
+  const vectorStore = await Chroma.fromExistingCollection(
+    new OpenAIEmbeddings(),
+    { collectionName: "makers",
+      url: "http://localhost:8000"}
+  );
+  
+  const response = await vectorStore.similaritySearch(currentMessageContent, 5);
 
-  if (previewToken) {
-    openai.apiKey = previewToken
-  }
+  const prompt = PromptTemplate.fromTemplate(TEMPLATE);
+ 
+  /**
+   * Chat models stream message chunks rather than bytes, so this
+   * output parser handles serialization and encoding.
+   */
+  const outputParser = new BytesOutputParser();
 
-  const res = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo',
-    messages,
-    temperature: 0.7,
-    stream: true
-  })
-
-  const stream = OpenAIStream(res, {
-    async onCompletion(completion) {
-      const title = json.messages[0].content.substring(0, 100)
-      const id = json.id ?? nanoid()
-      const createdAt = Date.now()
-      const path = `/chat/${id}`
-      const payload = {
-        id,
-        title,
-        userId,
-        createdAt,
-        path,
-        messages: [
-          ...messages,
-          {
-            content: completion,
-            role: 'assistant'
-          }
-        ]
-      }
-      await kv.hmset(`chat:${id}`, payload)
-      await kv.zadd(`user:chat:${userId}`, {
-        score: createdAt,
-        member: `chat:${id}`
-      })
-    }
-  })
-
-  return new StreamingTextResponse(stream)
+  /*
+   * Can also initialize as:
+   *
+   * import { RunnableSequence } from "langchain/schema/runnable";
+   * const chain = RunnableSequence.from([prompt, model, outputParser]);
+   */
+  const chain = prompt.pipe(ollama).pipe(outputParser);
+ 
+  const stream = await chain.stream({
+    query: currentMessageContent,
+    documents: JSON.stringify(response),
+    chat_history: formattedPreviousMessages.join('\n'),
+  });
+ 
+  return new StreamingTextResponse(stream);
 }
